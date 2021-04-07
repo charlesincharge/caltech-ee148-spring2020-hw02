d35ac6e558c3e4d9d18c803194a9f14b70f9e250
Charles Guan <cguan@caltech.edu>
2021-04-07 16:50:30 -0700

    bundle Python code into main function

--- generate_split.py
+++ generate_split.py
@@ -1,52 +1,56 @@
 import numpy as np
 import os
 
-np.random.seed(2020) # to ensure you always get the same train/test split
 
-data_path = '../data/RedLights2011_Medium'
-gts_path = '../data/hw02_annotations'
-split_path = '../data/hw02_splits'
-os.makedirs(preds_path, exist_ok=True) # create directory if needed
+def main():
+    np.random.seed(2020) # to ensure you always get the same train/test split
 
-split_test = False # set to True and run when annotations are available
+    data_path = '../data/RedLights2011_Medium'
+    gts_path = '../data/hw02_annotations'
+    split_path = '../data/hw02_splits'
+    os.makedirs(preds_path, exist_ok=True) # create directory if needed
 
-train_frac = 0.85
+    split_test = False # set to True and run when annotations are available
 
-# get sorted list of files:
-file_names = sorted(os.listdir(data_path))
+    train_frac = 0.85
 
-# remove any non-JPEG files:
-file_names = [f for f in file_names if '.jpg' in f]
+    # get sorted list of files:
+    file_names = sorted(os.listdir(data_path))
 
-# split file names into train and test
-file_names_train = []
-file_names_test = []
-'''
-Your code below. 
-'''
+    # remove any non-JPEG files:
+    file_names = [f for f in file_names if '.jpg' in f]
 
-assert (len(file_names_train) + len(file_names_test)) == len(file_names)
-assert len(np.intersect1d(file_names_train,file_names_test)) == 0
-
-np.save(os.path.join(split_path,'file_names_train.npy'),file_names_train)
-np.save(os.path.join(split_path,'file_names_test.npy'),file_names_test)
-
-if split_test:
-    with open(os.path.join(gts_path, 'annotations.json'),'r') as f:
-        gts = json.load(f)
-    
-    # Use file_names_train and file_names_test to apply the split to the
-    # annotations
-    gts_train = {}
-    gts_test = {}
+    # split file names into train and test
+    file_names_train = []
+    file_names_test = []
     '''
     Your code below. 
     '''
-    
-    with open(os.path.join(gts_path, 'annotations_train.json'),'w') as f:
-        json.dump(gts_train,f)
-    
-    with open(os.path.join(gts_path, 'annotations_test.json'),'w') as f:
-        json.dump(gts_test,f)
-    
-    
+
+    assert (len(file_names_train) + len(file_names_test)) == len(file_names)
+    assert len(np.intersect1d(file_names_train,file_names_test)) == 0
+
+    np.save(os.path.join(split_path,'file_names_train.npy'),file_names_train)
+    np.save(os.path.join(split_path,'file_names_test.npy'),file_names_test)
+
+    if split_test:
+        with open(os.path.join(gts_path, 'annotations.json'),'r') as f:
+            gts = json.load(f)
+
+        # Use file_names_train and file_names_test to apply the split to the
+        # annotations
+        gts_train = {}
+        gts_test = {}
+        '''
+        Your code below. 
+        '''
+
+        with open(os.path.join(gts_path, 'annotations_train.json'),'w') as f:
+            json.dump(gts_train,f)
+
+        with open(os.path.join(gts_path, 'annotations_test.json'),'w') as f:
+            json.dump(gts_test,f)
+
+
+if __name__ == '__main__':
+    main()
